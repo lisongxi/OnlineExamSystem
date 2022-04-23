3b034706dd113a3a0e1750131343efffe99a9b9d
LiSongXi <18813343309@163.com>
2022-04-23 23:03:02 +0800

    sixth time

--- src/main/java/com/lsx/onlineexamsystem/controller/StudentController.java
+++ src/main/java/com/lsx/onlineexamsystem/controller/StudentController.java
@@ -1,4 +1,51 @@
 package com.lsx.onlineexamsystem.controller;
 
+import com.lsx.onlineexamsystem.domain.Classe;
+import com.lsx.onlineexamsystem.domain.Student;
+import com.lsx.onlineexamsystem.mapper.ClasseMapper;
+import com.lsx.onlineexamsystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/student")
 public class StudentController {
+    @Autowired
+    private ClasseMapper classeMapper;
+    @Autowired
+    private StudentService studentService;
+    //查看所有学生
+    @RequestMapping("/getAllStudent")
+    public String getAllStudent(Model model){
+        List<Student> students = studentService.getAll();
+        model.addAttribute("students",students);
+        return "student/studentList";
+    }
+
+    //修改编辑功能,先获取该id得学生信息
+    @RequestMapping("/{id}")
+    public String updateStudent(@PathVariable("id") Integer id,Model model){
+        Student student=studentService.getStudentById(id);
+        List<Classe> classes = classeMapper.queryAll();
+        model.addAttribute("classes",classes);
+        model.addAttribute("student",student);
+        return "student/studentEdit";
+    }
+    //更改学生信息
+    @RequestMapping("/editStudent")
+    public String EditStudent(Student student){
+        studentService.EditStudent(student);
+        return "redirect:/student/getAllStudent";
+    }
+    //删除学生
+    @RequestMapping("/deleteStudent/{id}")
+    public String deleteStudent(@PathVariable("id") Integer id){
+        studentService.deleteById(id);
+        return "redirect:/student/getAllStudent";
+    }
 }
--- src/main/java/com/lsx/onlineexamsystem/mapper/StudentMapper.java
+++ src/main/java/com/lsx/onlineexamsystem/mapper/StudentMapper.java
@@ -1,4 +1,34 @@
 package com.lsx.onlineexamsystem.mapper;
 
+import com.lsx.onlineexamsystem.domain.Student;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
 public interface StudentMapper {
+    List<Student> queryAll();
+
+    Student check(Student student);
+
+    @Select("select count(*) from student")
+    int queryCOuntALlstu();
+
+    void AddStudent(Student student);
+
+    Student getStudentById(Integer id);
+
+    void EditStudent(Student student);
+
+    @Delete("delete from student where student_id=#{id}")
+    void deleteById(Integer id);
+
+    String queryNotice();
+
+    void addNotice(Student student);
 }
